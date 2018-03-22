a656a01935afbb3909530bb5357f9b481d4c2a17
Izzy Davidowitz <srulydavid@gmail.com>
2018-03-21 21:25:41 -0400

    added axios

--- app.js/app.js
+++ app.js/app.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express()
 const request = require('request')
+const axios = require('axios')
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+async function getDataFromFitbit(){
+  const myData = await axios.get('https://www.bhphotovideo.com/c/json/search/?N=0&InitialSearch=yes&sts=ma&Top+Nav-Search&Ntt=sony%20alpha%20a7%20iii%2024');
+  console.log(myData);
+  return myData;
+}
 
+getDataFromFitbit();
 
-
+/*
 
 app.get('/fitbitData', function (req, res) {
 	request.get("https://www.bhphotovideo.com/c/json/search/?N=0&InitialSearch=yes&sts=ma&Top+Nav-Search&Ntt=sony%20alpha%20a7%20iii%2024", function(error, response, body){
 		res.json(body);
 	}
  	})
-
-//  res.send('Hello World!')
+*/
 					
-
-
-
+/*
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+	});
+*/
