e266fa4b5e855fe581c6624b8b764130e041b78d
Izzy Davidowitz <srulydavid@gmail.com>
2018-03-21 20:28:49 -0400

    trying to add an api call

--- app.js/app.js
+++ app.js/app.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
+const request = require('request')
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+
+
+
+app.get('/fitbitData', function (req, res) {
+	request.get("https://www.bhphotovideo.com/c/json/search/?N=0&InitialSearch=yes&sts=ma&Top+Nav-Search&Ntt=sony%20alpha%20a7%20iii%2024", function(error, response, body){
+		res.json(body);
+	}
+ 	})
+
+//  res.send('Hello World!')
+					
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
