0e204a0f12125ff36d6884ad702f9ee449293653
Izzy Davidowitz <srulydavid@gmail.com>
2018-04-12 20:41:56 -0400

    added blah blah

--- app/app.js
+++ app/app.js
@@ -2,23 +2,50 @@ const express = require('express')
 const app = express()
 const request = require('request')
 const axios = require('axios')
-
+const date = require('date')
 
 async function getDataFromFitbit(){
-  const myData = await axios.get('https://www.bhphotovideo.com/c/json/search/?N=0&InitialSearch=yes&sts=ma&Top+Nav-Search&Ntt=sony%20alpha%20a7%20iii%2024');
+  const myData = await axios.get('https://www.bhphotovideo.com/bnh/controller/home?A=getItemDetail&Q=json&O=&sku=1394217&is=REG&show=mainInfo+images+itemGrouping+qa+breadcrumbs');
   console.log(myData);
-  return myData;
+  return myData.data;
 }
 
 //getDataFromFitbit();
 
 
-
 app.get('/', function(req, res) {
-	getDataFromFitbit().then(function(data){
-		res.json(data);
-	});
+  res.send(`
+   <!DOCTYPE html>
+<html>
+<body>
+
+<h1>Welcome</h1>
+
+<p>click <a href="/time">here</a> for time</p>
+
+</body>
+</html>
+      
+  `);
 });
+
+
+app.get('/time', function(req, res) {
+  res.send(`
+   <!DOCTYPE html>
+<html>
+<body>
+
+<h1>Welcome to time page</h1>
+
+<p>current time goes here</p>
+
+</body>
+</html>
+      
+  `);
+});
+
 
 
 app.listen(3000, function () {
